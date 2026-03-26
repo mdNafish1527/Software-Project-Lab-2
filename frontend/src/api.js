348dd80b074c48902be9e67bf2bd4b3d251275cb
mdNafish1527 <166948117+mdNafish1527@users.noreply.github.com>
2026-03-26 18:00:02 +0600

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = axios.create({ baseURL: '/api' });
 
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = sessionStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
